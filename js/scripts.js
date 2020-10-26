e16dcaceb2f68169e27ce8d5764d3f07e3c3aabd
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-10-26 14:48:27 -0700

    add working loop to collect inputs
    Co-authored-by: Austin Schrader <austin.jschrader@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,34 +6,17 @@ $(document).ready(function() {
 
     let newInputs = [];
 
-    // For each input the user wrote down, grab its value
     inputs.forEach(function(input) {
       let userInput = $(("input" + input)).val();
       newInputs.push(userInput);
-      // console.log(testResult)
     });
 
-    let person1Input = newInputs[0];
-    let person2Input = newInputs[1];
-    let animalInput = newInputs[2];
-    let exclamationInput = newInputs[3];
-    let verbInput = newInputs[4];
-    let nounInput = newInputs[5]; 
-
-    // const words = [person1Input, person2Input, animalInput, exclamationInput, verbInput, nounInput];
-
-    const outputs = [".person1", ".person2", ".animal", ".exclamation", ".verb", ".noun"];
-    outputs.forEach(function(output) {
-      // $(".person1").text(newInput);
-      $(output).text(person1Input);
-    });
-
-    // $(".person1").text(person1Input);
-    // $(".person2").text(person2Input);
-    // $(".animal").text(animalInput);
-    // $(".exclamation").text(exclamationInput);
-    // $(".verb").text(verbInput);
-    // $(".noun").text(nounInput);
+    $(".person1").text(newInputs[0]);
+    $(".person2").text(newInputs[1]);
+    $(".animal").text(newInputs[2]);
+    $(".exclamation").text(newInputs[3]);
+    $(".verb").text(newInputs[4]);
+    $(".noun").text(newInputs[5]);
 
     $("#story").show();
   });

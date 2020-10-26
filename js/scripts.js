4c56e8cc8f089b98c69c548356e2c6c563afb44d
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-10-26 14:18:04 -0700

    adds user input into story
    Co-authored-by: Austin Schrader <austin.jschrader@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,39 +2,39 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
 
-    // Declare the variables
-    let person1Input = "";
-    let person2Input = "";
-    let animalInput = "";
-    let exclamationInput = "";
-    let verbInput = "";
-    let nounInput = ""; 
-
-    // Put all the inputs into an array called inputs
     const inputs = ["#person1", "#person2", "#animal", "#exclamation", "#verb", "#noun"];
 
-    // Use a for loop to grab the inputted value for each of the variables
+    let newInputs = [];
+
+    // For each input the user wrote down, grab its value
     inputs.forEach(function(input) {
-      let testResult = $(("input" + input)).val();
-      console.log(testResult)
+      let userInput = $(("input" + input)).val();
+      newInputs.push(userInput);
+      // console.log(testResult)
     });
 
-    // What we're trying to do
-    // Each one of the inputs are put into the spans
+    let person1Input = newInputs[0];
+    let person2Input = newInputs[1];
+    let animalInput = newInputs[2];
+    let exclamationInput = newInputs[3];
+    let verbInput = newInputs[4];
+    let nounInput = newInputs[5]; 
 
     // const words = [person1Input, person2Input, animalInput, exclamationInput, verbInput, nounInput];
 
-    // words.forEach(function(word) {
-    //   $(".person1").text(word);
-    // });
+    const outputs = [".person1", ".person2", ".animal", ".exclamation", ".verb", ".noun"];
+    outputs.forEach(function(output) {
+      // $(".person1").text(newInput);
+      $(output).text(person1Input);
+    });
 
-    // // $(".person1").text(person1Input);
-    // // $(".person2").text(person2Input);
-    // // $(".animal").text(animalInput);
-    // // $(".exclamation").text(exclamationInput);
-    // // $(".verb").text(verbInput);
-    // // $(".noun").text(nounInput);
+    // $(".person1").text(person1Input);
+    // $(".person2").text(person2Input);
+    // $(".animal").text(animalInput);
+    // $(".exclamation").text(exclamationInput);
+    // $(".verb").text(verbInput);
+    // $(".noun").text(nounInput);
 
-    // $("#story").show();
+    $("#story").show();
   });
 });

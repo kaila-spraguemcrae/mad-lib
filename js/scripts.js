50d0c82481f84ea49daa99edcb4fd6c2a190a63e
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-10-26 13:39:47 -0700

    add input array and loop
    Co-authored-by: Austin Schrader <austin.jschrader@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,40 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
-    const person1Input = $("input#person1").val();
-    console.log(person1Input);
-    const person2Input = $("input#person2").val();
-    const animalInput= $("input#animal").val();
-    const exclamationInput = $("input#exclamation").val();
-    const verbInput = $("input#verb").val();
-    const nounInput = $("input#noun").val();
 
-    $("person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    // Declare the variables
+    let person1Input = "";
+    let person2Input = "";
+    let animalInput = "";
+    let exclamationInput = "";
+    let verbInput = "";
+    let nounInput = ""; 
 
-    $("#story").show();
+    // Put all the inputs into an array called inputs
+    const inputs = ["#person1", "#person2", "#animal", "#exclamation", "#verb", "#noun"];
+
+    // Use a for loop to grab the inputted value for each of the variables
+    inputs.forEach(function(input) {
+      let testResult = $(("input" + input)).val();
+      console.log(testResult)
+    });
+
+    // What we're trying to do
+    // Each one of the inputs are put into the spans
+
+    // const words = [person1Input, person2Input, animalInput, exclamationInput, verbInput, nounInput];
+
+    // words.forEach(function(word) {
+    //   $(".person1").text(word);
+    // });
+
+    // // $(".person1").text(person1Input);
+    // // $(".person2").text(person2Input);
+    // // $(".animal").text(animalInput);
+    // // $(".exclamation").text(exclamationInput);
+    // // $(".verb").text(verbInput);
+    // // $(".noun").text(nounInput);
+
+    // $("#story").show();
   });
 });

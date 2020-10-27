9549443b304db1f576332622fc2e06581b79814a
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-10-26 18:25:39 -0700

    add loop which puts user input into story

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,21 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
 
-    const inputs = ["#person1", "#person2", "#animal", "#exclamation", "#verb", "#noun"];
+    const inputs = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
     let newInputs = [];
 
     inputs.forEach(function(input) {
-      let userInput = $(("input" + input)).val();
+      let userInput = $(("input#" + input)).val();
       newInputs.push(userInput);
     });
 
-    $(".person1").text(newInputs[0]);
-    $(".person2").text(newInputs[1]);
-    $(".animal").text(newInputs[2]);
-    $(".exclamation").text(newInputs[3]);
-    $(".verb").text(newInputs[4]);
-    $(".noun").text(newInputs[5]);
+    let indexNum = 0;
+
+    inputs.forEach(function(input){
+      $("." + input).text(newInputs[indexNum]);
+      indexNum += 1;
+    });
 
     $("#story").show();
   });
